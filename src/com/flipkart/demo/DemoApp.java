ed818a367df0da41c6ac88446abc6200245af680
Piyush Soni <piyush.soni@BLRETV-TXM43XDK.local>
2022-07-22 10:07:36 +0530

    first java commit

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author piyush.soni
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("this is my demo project");
+	}
+
+}
